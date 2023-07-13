cca73fd7412b8cddd4aff496082bf22d7d08ebab
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-07-13 19:12:21 +0300

    initial commit

---
+++ main.sh
@@ -0,0 +1,12 @@
+cd ./pika-apx-configs
+
+# Get build deps
+apt-get build-dep ./ -y
+
+# Build package
+dpkg-buildpackage --no-sign
+
+# Move the debs to output
+cd ../
+mkdir -p ./output
+mv ./*.deb ./output/
